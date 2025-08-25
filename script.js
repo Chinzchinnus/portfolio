74d57b5ee812f2bfa138acacd734a5a150f71a91
Chinzchinnus <91425735+Chinzchinnus@users.noreply.github.com>
2025-08-25 21:17:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelectorAll('nav a').forEach(link => {
 const darkModeBtn = document.getElementById('darkModeBtn');
 darkModeBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
-  
+
   // Change button text/icon
   if (document.body.classList.contains('dark-mode')) {
     darkModeBtn.textContent = "☀️ Light Mode";
